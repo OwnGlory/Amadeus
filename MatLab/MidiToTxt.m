b7fb12ca29cdbc26e72db5e76d759edaff61b7f5
Vlad_Work <inf.vlad.s@bk.ru>
2024-01-30 10:42:41 +0300

    push

--- MatLab/MidiToTxt.m
+++ MatLab/MidiToTxt.m
@@ -1,7 +1,9 @@
 %addpath(genpath('D:\Develop\Amadeus\matlab-midi-master\src'))
-
+% Чтение MIDI файла
 midiFile = readmidi('All_I_Need.mid');
+% Получение информации из MIDI
 midiData = midiInfo(midiFile);
+% Вывод MIDI данных
 disp(midiData);
 % Указываем имя файла
 filename = 'output.txt';
